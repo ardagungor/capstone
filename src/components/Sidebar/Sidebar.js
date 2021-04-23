76cd06f075f705468cde5e66968b4210736efe29
ardagungor98 <ardagungor98@gmail.com>
2021-04-23 13:49:04 +0300

    sidebar design changed

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -7,7 +7,7 @@ import { GrDocumentPerformance } from "react-icons/gr";
 const Sidebar = () => {
   return (
     <div>
-      <div className={classes.container}>
+      {/* <div className={classes.container}>
         <NavLink to="/">
           <h1 className={classes.sidebar_h1}>Capstone 1220</h1>
         </NavLink>
@@ -67,6 +67,67 @@ const Sidebar = () => {
             </NavLink>
           </li>
         </ul>
+      </div> */}
+      <div className={classes.container}>
+        <div className={classes.navigation}>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className={classes.sidebar_li}
+                activeClassName={classes.activeLink}
+              >
+                <span className={classes.icon}></span>
+                <span className={classes.title}>Capstone 1220</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/order"
+                className={classes.sidebar_li}
+                activeClassName={classes.activeLink}
+              >
+                <span className={classes.icon}>
+                  {" "}
+                  <MdBorderColor
+                  // style={{
+                  //   width: "30px",
+                  //   height: "30px",
+                  //   marginLeft: "auto",
+                  //   marginRight: "auto",
+                  //   paddingLeft: "5px",
+                  // }}
+                  />
+                </span>
+                <span className={classes.title}>Order</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/home"
+                className={classes.sidebar_li}
+                activeClassName={classes.activeLink}
+              >
+                <span className={classes.icon}>
+                  <MdBorderColor />
+                </span>
+                <span className={classes.title}>Order Summary</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/results"
+                className={classes.sidebar_li}
+                activeClassName={classes.activeLink}
+              >
+                <span className={classes.icon}>
+                  <MdBorderColor />
+                </span>
+                <span className={classes.title}>Results</span>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
